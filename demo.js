340ae3e3ddc56074c460def342e557803a36e9f5
prasadkadam6718 <prasadkadam6718@gmail.com>
2024-06-25 22:03:53 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,203 @@
+// console.error("areeee.... error aa gaya.....")
+
+// console.warn("tum jo ye kar rahe ho na.. wo bikul sahi nahi hai")
+
+// alert("kaise ho.... bade dino bad dikhayi diye coding karte hue.....")
+
+
+
+// console.log()
+
+// if (false && false && false && false && false) {
+//   console.log("either one values is true")
+// }
+
+
+
+
+
+
+
+
+// var a = 10;
+// console.log(a)
+
+// a = 20
+// console.log(a)
+
+
+
+
+// let a = 10
+// console.log(a)
+
+
+// let a;
+// a = 20
+// console.log(a)
+
+// let a;
+
+
+
+// let a = 10
+// a = 20
+// a = 30
+// console.log(a)
+
+
+
+
+// const c = 10
+// console.log(c)
+
+
+// const c = 10;
+// c = 20;
+
+
+
+
+
+// let a = 10n
+// console.log("type of a is ", typeof a)
+
+
+
+// console.log(9999999999999999)
+
+// let a = 1n
+// console.log(a)
+
+// let c = BigInt(10)
+// console.log(c)
+
+
+
+
+
+
+// console.log(10 / "a")
+// console.log(typeof NaN)
+
+
+
+// let name = "Tinku"
+// let friend = 'Tinki'
+// let tinkiKaBf = `Pinku`
+
+
+// let intro = 'My name is "Tinku".!'
+// console.log(intro)
+// let woo = "Tinki"
+// let intro = `my name is Tinku
+// and i live in Noida
+// or mai bhindi khata hu
+// or mai paanin bhi pita hu
+// or wo maine pani pi pi kar jo khun banaya usko ${woo} aakar pi jati hai... `
+// console.log(intro)
+
+
+
+
+// let func = () => "hello"
+// console.log(func())
+
+// console.log(typeof this)
+
+
+// let obj1 = {
+//   name: "prasad"
+// }
+// vv
+
+// obj1.__proto__ = obj2
+
+// console.log(obj1.surname)
+
+
+// let obj = {
+//   name: "prasad",
+//   surname: "Kadam",
+//   location: "Noida"
+// }
+
+
+
+
+
+
+
+// let isLoggedIn = false
+
+
+// console.log(Boolean())
+
+
+
+
+
+// let a = ""
+// console.log(a)
+
+
+
+// let name = prompt("enter your name")
+// console.log(name)
+
+
+// let price = 100
+// let name = "Tshirt"
+
+// let productInfo = `the name of the product is ${name}. and its price is ${price}/- rs only.`
+
+// console.log(productInfo)
+
+
+
+
+// symbol
+
+// let s1 = Symbol(10)
+// let s2 = Symbol(10)
+
+// // let s1 = 10
+// // let s2 = 10
+
+// console.log(s1 == s2)
+
+
+
+
+// Non primitive data types
+
+// Functions
+
+// function bookticket(location) {
+//   console.log(`Ticket booked for ${location} ....!`)
+// }
+// bookticket("Manali")
+
+
+
+
+console.log("mai ghar se nikla")
+function shadi(gift) {
+  console.log(`shadi me khana khane chala gaya ${gift} lekar.`)
+}
+console.log("dost ke ghar gaya")
+shadi("flowers")
+shadi("flowers")
+shadi("flowers")
+shadi("flowers")
+shadi("flowers")
+shadi("flowers")
+
+
+
+
+
+
+
+
+
